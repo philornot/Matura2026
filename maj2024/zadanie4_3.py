07a2701b94542a9dfdedb7d6974fea1c7c2d7862
philornot <philornot@users.noreply.github.com>
2025-10-02 16:44:41 +0200

    usuń duplikat funkcji wczytaj_dane

--- maj2024/zadanie4_3.py
+++ maj2024/zadanie4_3.py
@@ -1,17 +1,4 @@
-def wczytaj_dane(przyklad=False):
-    if przyklad:
-        plik = 'liczby_przyklad.txt'
-    else:
-        plik = 'liczby.txt'
-    with open(plik, 'r') as f:
-        wiersz1_raw, wiersz2_raw = f.readlines()
-    wiersz1, wiersz2 = [], []
-    wiersz1_raw, wiersz2_raw = wiersz1_raw.split(), wiersz2_raw.split()
-    for i in wiersz1_raw:
-        wiersz1.append(int(i))
-    for j in wiersz2_raw:
-        wiersz2.append(int(j))
-    return wiersz1, wiersz2
+from wczytaj_dane import wczytaj_dane
 
 
 def rozklad_na_czynniki(liczba):
