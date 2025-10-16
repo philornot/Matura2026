e30c267bfec7a2667b9cfe251cc7829806c3280c
philornot <philornot@users.noreply.github.com>
2025-10-16 16:57:52 +0200

    Możliwe że to nie jest najbardziej zoptymalizowany kod, ale działa :>

--- maj2024/zadanie4_4.py
+++ maj2024/zadanie4_4.py
@@ -1,21 +1,24 @@
 from wczytaj_dane import wczytaj_dane
 
 w1, w2 = wczytaj_dane(przyklad=False)
-maks_srednia = float('-inf')
-maks_dlugosc = 0
-maks_poczatek = 0
+
+najwieksza_srednia = 0
+dlugosc_fragmentu = 0
+pierwszy_element = 0
 
 for start in range(len(w1)):
-    suma = 0
     for dlugosc in range(50, len(w1) - start + 1):
-        suma += w1[start + dlugosc - 1]
+
+        suma = 0
+        for i in range(start, start + dlugosc):
+            suma += w1[i]
+
         srednia = suma / dlugosc
-        if srednia > maks_srednia:
-            maks_srednia = srednia
-            maks_dlugosc = dlugosc
-            maks_poczatek = start
 
-print(f"{maks_srednia} {maks_dlugosc} {w1[maks_poczatek]}")
-odpowiedz4 = f"{maks_srednia} {maks_dlugosc} {w1[maks_poczatek]}"
+        if srednia > najwieksza_srednia:
+            najwieksza_srednia = srednia
+            dlugosc_fragmentu = dlugosc
+            pierwszy_element = w1[start]
 
-# dla przykładu wychodzi zła odpowiedź (3.905 200 2), ale nie wiem co jest źle
+print(f"{najwieksza_srednia} {dlugosc_fragmentu} {pierwszy_element}")
+odpowiedz4 = f"{najwieksza_srednia} {dlugosc_fragmentu} {pierwszy_element}"
