400cf887384f7a44e150ea5ab347e5687f3c663a
philornot <philornot@users.noreply.github.com>
2026-02-06 21:13:39 +0100

    4.2 zrobione od dołu

--- grudzien2022/zadanie2/zadanie2_4.py
+++ grudzien2022/zadanie2/zadanie2_4.py
@@ -13,7 +13,27 @@ def wczytaj_pary():
     return pary
 
 
-pary = wczytaj_pary()
-print(pary)
+def ile_strzalek(y, x):  # idąc od dołu drzewa
+    while y > x:
+        y = y // 2
+    return y == x
 
-# i dalej nie mam pojęcia co zrobić :/
+
+pary = wczytaj_pary()
+# print(pary)
+
+
+licznik = 0
+for para in pary:
+    x, y = para
+    if ile_strzalek(y, x) >= 1:
+        print(x, y)
+        licznik += 1
+print(f'\nŁącznie {licznik} znalezionych par (wszystkich {len(pary)})')
+
+# 47 3044
+# 7650 61204
+# 1 245
+# 7 63669
+# 9125 18250
+# 5 43246
