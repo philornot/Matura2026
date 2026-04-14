a6900fa7db90bfe82828295d7c57124fd2587416
philornot <philornot@users.noreply.github.com>
2026-04-14 23:42:47 +0200

    okej więc okazuje się że kod jest poprawny tylko dane są złe.

--- operonSystemyLiczbowe/18_1.py
+++ operonSystemyLiczbowe/18_1.py
@@ -8,35 +8,17 @@ def wczytaj_dane():
         dane.append(liczby)
     return dane
 
-dziala_licznik = nie_dziala_licznik = 0
+
 def znajdz_system(liczby):
     systemy_litery = {'A': 11, 'B': 12, 'C': 13, 'D': 14, 'E': 15, 'F': 16, 'G': 17}
-    for i in range(1,10):
-        systemy_litery[str(i)] = i +1
-    #print(systemy_litery)
+    for i in range(0,11):
+        systemy_litery[str(i)] = i + 1
     lacznie = []
     for liczba in liczby:
         for znak in liczba:
             lacznie.append(ord(znak))
     maks_cyfra = max(lacznie)
     system = systemy_litery[chr(maks_cyfra)]
-
-    global dziala_licznik, nie_dziala_licznik
-    dziala = False
-    if int(trzy_liczby[0][2], system) != int(trzy_liczby[0][0],system) + int(trzy_liczby[0][1],system):
-        nie_dziala_licznik += 1
-        dziala = False
-    else:
-        dziala_licznik += 1
-        dziala = True
-
-    while not dziala: # brute force
-        system += 1
-        if int(trzy_liczby[0][2], system) != int(trzy_liczby[0][0], system) + int(trzy_liczby[0][1], system):
-            dziala = False
-        else:
-            dziala = True
-
     return system
 
 
@@ -44,7 +26,7 @@ wystepowania = {}
 dane = wczytaj_dane()
 for trzy_liczby in dane:
     system = znajdz_system(trzy_liczby)
-    print(trzy_liczby, system)
+    # print(trzy_liczby, system)
     if system not in wystepowania:
         wystepowania[system] = 1
     else:
@@ -54,5 +36,3 @@ print()
 
 for system in sorted(wystepowania.keys()):
     print(f'System {system} - {wystepowania[system]} wystąpień')
-
-print(f'Funkcja zadziałała w {dziala_licznik} przypadkach na 100 (nie zadziałała w {nie_dziala_licznik})')
